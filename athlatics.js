383071bb7f8dc475291ecde211f8a21541866167
Nikethapawar-19 <nikethapawar1995@gmail.com>
2022-11-22 12:24:32 +0530

    changed

--- athlatics.js
+++ athlatics.js
@@ -2,7 +2,7 @@ function relay(){
     console.log("this is relay");
 }
 function highjump(){
-    console.log("this is highjump");
+    console.log("this is highjump!!!!");
 }
 module.exports.relay=relay;
 module.exports.highjump=highjump;
